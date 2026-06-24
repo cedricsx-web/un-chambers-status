ddf3397bf7b9dd627a170f24cc35fa518721fc38
cedricsx-web <cedric.stcroix@gmail.com>
2026-06-24 12:04:57 -0400

    Change base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/un-daily-briefing/',   // ← must match your GitHub repo name
+  base: '/un-chambers-status/',   // ← must match your GitHub repo name
 })
